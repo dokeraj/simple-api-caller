2ce7f9d5be410d57dbbfc81c73a5b16f67952636
dokeraj <dokeraj87@gmail.com>
2024-06-26 20:36:08 +0200

    added timeout and exits the container once the api is called

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+import sys
+
 import requests
 
 import configInit
+
+API_TIMEOUT = int(os.environ.get('API_TIMEOUT', 3))
 
 
 def runRequests(config):
@@ -11,21 +16,23 @@ def runRequests(config):
                 method=req.http_method,
                 headers=req.converted_headers,
                 json=req.payload
-            )
+                , timeout=(3, API_TIMEOUT))
 
             if 200 <= response.status_code <= 299:
                 print(f"Successfully called: ${response.json()}")
             else:
                 print(f"API call failed with status code {response.status_code} and error: ${response.json()}")
 
+            sys.exit()
+
         except requests.exceptions.Timeout:
-            strErr = f"ERROR: Timeout has occurred while calling scanning folder(s)! Please run the scan manually!"
+            strErr = f"ERROR: Timeout has occurred after {API_TIMEOUT} seconds, while making request: {req.url}! Exiting app!"
             print(strErr)
-            return None
+            sys.exit()
         except Exception as e:
-            strErr = f"ERROR: {str(e)} has occurred while calling the API: ${req.url}!"
+            strErr = f"ERROR: {str(e)} has occurred while calling the API: ${req.url}! Exiting app!"
             print(strErr)
-            return None
+            sys.exit()
 
 
 def main():
